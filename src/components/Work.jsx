60584e8afdca2eb0cd6a561d6f505e4513760687
qezman <holaryinka5050@gmail.com>
2024-01-04 23:33:31 +0100

    updated work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -14,6 +14,78 @@ const Work = () => {
           <p className={"py-6"}> Check out some of my recent work</p>
         </div>
 
+
+        {/*UI/UX PROJECTS Container-2*/}
+        <h1 className={"text-xl text-blue-500 text-center font-semibold mt-3 py-4"}>UI/UX Projects</h1>
+
+        <div
+          className={"grid sm:grid-cols-2 md:grid-cols-3 gap-4"}>
+          {/*Grid item*/}
+          <div
+            style={{backgroundImage: `url(${furniImg})`}}
+            className={"shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"}>
+            {/*Hover Effects*/}
+            <div className={"opacity-0 group-hover:opacity-100"}>
+              <span className={"text-2xl font-bold text-white tracking-wider"}>
+              Behance Project
+              </span>
+              <div className={"pt-8 text-center"}>
+                <a href="https://www.behance.net/gallery/182235163/Furniture-Landing-Page"
+                   className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
+                  <button>Demo</button>
+                </a>
+
+                {/* <a href="/"
+                   className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
+                  <button>Code</button>
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{backgroundImage: `url(${gtcoImg})`}}
+            className={"shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"}>
+            {/*Hover Effects*/}
+            <div className={"opacity-0 group-hover:opacity-100"}>
+              <span className={"text-2xl font-bold text-white tracking-wider"}>
+                Behance Project
+              </span>
+              <div className={"pt-8 text-center"}>
+                <a href="https://www.behance.net/gallery/186960521/GTCO-Redesign-Mobile-App" className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
+                  <button>Demo</button>
+                </a>
+{/* 
+                <a href="/" className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
+                  <button>Code</button>
+                </a> */}
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{backgroundImage: `url(${educationImg})`}}
+            className={"shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"}>
+            {/*Hover Effects*/}
+            <div className={"opacity-0 group-hover:opacity-100"}>
+              <span className={"text-2xl font-bold text-white tracking-wider"}>
+              Behance Project
+              </span>
+              <div className={"pt-8 text-center"}>
+                <a href="https://www.behance.net/gallery/181787301/E-learning-Mobile-App"
+                   className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
+                  <button>Demo</button>
+                </a>
+
+                {/* <a href="/"
+                   className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
+                  <button>Code</button>
+                </a> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
         {/*Container*/}
         <h1 className={"text-xl text-center font-semibold mt-3 py-4 text-blue-500"}>React Projects</h1>
 
@@ -84,78 +156,6 @@ const Work = () => {
             </div>
           </div>
         </div>
-
-        {/*Container-2*/}
-        <h1 className={"text-xl text-blue-500 text-center font-semibold mt-3 py-4"}>UI/UX Projects</h1>
-
-        <div
-          className={"grid sm:grid-cols-2 md:grid-cols-3 gap-4"}>
-          {/*Grid item*/}
-          <div
-            style={{backgroundImage: `url(${furniImg})`}}
-            className={"shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"}>
-            {/*Hover Effects*/}
-            <div className={"opacity-0 group-hover:opacity-100"}>
-              <span className={"text-2xl font-bold text-white tracking-wider"}>
-              Figma Project
-              </span>
-              <div className={"pt-8 text-center"}>
-                <a href="https://www.behance.net/gallery/182235163/Furniture-Landing-Page"
-                   className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
-                  <button>Demo</button>
-                </a>
-
-                {/* <a href="/"
-                   className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
-                  <button>Code</button>
-                </a> */}
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{backgroundImage: `url(${gtcoImg})`}}
-            className={"shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"}>
-            {/*Hover Effects*/}
-            <div className={"opacity-0 group-hover:opacity-100"}>
-              <span className={"text-2xl font-bold text-white tracking-wider"}>
-                Figma Project
-              </span>
-              <div className={"pt-8 text-center"}>
-                <a href="https://www.behance.net/gallery/186960521/GTCO-Redesign-Mobile-App" className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
-                  <button>Demo</button>
-                </a>
-{/* 
-                <a href="/" className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
-                  <button>Code</button>
-                </a> */}
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{backgroundImage: `url(${educationImg})`}}
-            className={"shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"}>
-            {/*Hover Effects*/}
-            <div className={"opacity-0 group-hover:opacity-100"}>
-              <span className={"text-2xl font-bold text-white tracking-wider"}>
-              Figma Project
-              </span>
-              <div className={"pt-8 text-center"}>
-                <a href="https://www.behance.net/gallery/181787301/E-learning-Mobile-App"
-                   className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
-                  <button>Demo</button>
-                </a>
-
-                {/* <a href="/"
-                   className={"text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg"}>
-                  <button>Code</button>
-                </a> */}
-              </div>
-            </div>
-          </div>
-        </div>
-
 
 
       </div>
