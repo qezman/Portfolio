f74b425677740f2a60b72eaccca562116514c905
qezman <holaryinka5050@gmail.com>
2024-01-04 23:49:19 +0100

    updated colors

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,47 +16,47 @@ const Skills = () => {
       <div className={"max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full"}>
         <div>
           <p className={"text-4xl font-bold inline border-b border-blue-500"}>Skills</p>
-          <p>These are the technologies I've worked with</p>
+          <p>These are some of the technologies I've worked with</p>
         </div>
 
         <div className={"w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8"}>
           <div className={"shadow-md shadow-[#040c16] hover:scale-110 duration-500"}>
-            <FaHtml5 className={"w-20 mx-auto"} />
+            <FaHtml5 className={"text-[#E44D26] w-20 mx-auto"} size={50} />
             <p className={"my-4"}>HTML 5</p>
           </div>
 
           <div className={"shadow-md shadow-[#040c16] hover:scale-110 duration-500"}>
-            <FaCss3 className={"w-20 mx-auto"} />
+            <FaCss3 className={"text-[#1572B6] w-20 mx-auto"} size={50} />
             <p className={"my-4"}>CSS</p>
           </div>
 
           <div className={"shadow-md shadow-[#040c16] hover:scale-110 duration-500"}>
-            <DiJavascript className={"w-20 mx-auto"} />
+            <DiJavascript className={"text-[#F7DF1E] w-20 mx-auto"} size={50} />
             <p className={"my-4"}>JAVASCRIPT</p>
           </div>
 
           <div className={"shadow-md shadow-[#040c16] hover:scale-110 duration-500"}>
-            <FaReact className={"w-20 mx-auto"} />
+            <FaReact className={"text-[#61DAFB] w-20 mx-auto"} size={50} />
             <p className={"my-4"}>REACT</p>
           </div>
 
           <div className={"shadow-md shadow-[#040c16] hover:scale-110 duration-500"}>
-            <SiTailwindcss className={"w-20 mx-auto"} />
+            <SiTailwindcss className={"text-[#6366F1] w-20 mx-auto"} size={50} />
             <p className={"my-4"}>Tailwind CSS</p>
           </div>
 
           <div className={"shadow-md shadow-[#040c16] hover:scale-110 duration-500"}>
-            <FaFigma className={"w-20 mx-auto"} />
+            <FaFigma className={"text-white w-20 mx-auto"} size={50} />
             <p className={"my-4"}>FIGMA</p>
           </div>
 
           <div className={"shadow-md shadow-[#040c16] hover:scale-110 duration-500"}>
-            <SiAdobeillustrator className={"w-20 mx-auto"} />
+            <SiAdobeillustrator className={"text-orange-500 w-20 mx-auto"} size={50} />
             <p className={"my-4"}>ILLUSTRATOR</p>
           </div>
 
           <div className={"shadow-md shadow-[#040c16] hover:scale-110 duration-500"}>
-            <SiAdobexd className={"w-20 mx-auto"} />
+            <SiAdobexd className={"text-pink-500 w-20 mx-auto"} size={50} />
             <p className={"my-4"}>ADOBE XD</p>
           </div>
         </div>
